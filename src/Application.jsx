a1bb6d7b491e0f4486ca950788a986692124f2c9
Rhino <rhinoandre@gmail.com>
2022-11-16 09:16:27 -0300

    fix: remove query

--- src/Application.jsx
+++ src/Application.jsx
@@ -5,14 +5,6 @@ import Home from './Home';
 class Application extends Nullstack {
   nftChildren;
 
-  static async getNFTChildren({ database }) {
-    return await database.collection('children_nft').find().toArray();
-  }
-
-  async initiate() {
-    this.nftChildren = await this.getNFTChildren();
-  }
-
   prepare({ page }) {
     page.locale = 'en-US';
   }
@@ -33,18 +25,6 @@ class Application extends Nullstack {
     return (
       <body class="bg-gray-900 text-white font-roboto">
         <Head />
-        {this.nftChildren?.map(child => (<>
-          <div>
-            <h1>{child.name}</h1>
-            <p>{child.nftCode}</p>
-            <p>{child.description}</p>
-          </div>
-          <div>
-            <h1>{child.nftForDonation.name}</h1>
-            <p>{child.nftForDonation.nftCode}</p>
-            <p>{child.nftForDonation.description}</p>
-          </div>
-        </>))}
         <Home route="/" greeting="Welcome to Nullstack!" />
       </body>
     )
