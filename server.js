845bf7a6580a38fc379e25dd42128a5236902d57
Rhino <rhinoandre@gmail.com>
2022-11-16 08:42:36 -0300

    feat: add DB connection

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+import { MongoClient } from 'mongodb';
 import Nullstack from 'nullstack';
 import Application from './src/Application';
 
 const context = Nullstack.start(Application);
 
 context.start = async function start() {
-  // https://nullstack.app/application-startup
+  const { secrets } = context;
+
+  const databaseClient = new MongoClient(secrets.databaseHost);
+  await databaseClient.connect();
+  context.database = await databaseClient.db(secrets.databaseName);
 }
 
 export default context;
--- src/Application.jsx
+++ src/Application.jsx
@@ -3,6 +3,15 @@ import '../tailwind.css';
 import Home from './Home';
 
 class Application extends Nullstack {
+  nftChildren;
+
+  static async getNFTChildren({ database }) {
+    return await database?.collection('children_nft').find().toArray();
+  }
+
+  async initiate(context) {
+    this.nftChildren = await Application.getNFTChildren(context);
+  }
 
   prepare({ page }) {
     page.locale = 'en-US';
@@ -24,6 +33,18 @@ class Application extends Nullstack {
     return (
       <body class="bg-gray-900 text-white font-roboto">
         <Head />
+        {this.nftChildren?.map(child => (<>
+          <div>
+            <h1>{child.name}</h1>
+            <p>{child.nftCode}</p>
+            <p>{child.description}</p>
+          </div>
+          <div>
+            <h1>{child.nftForDonation.name}</h1>
+            <p>{child.nftForDonation.nftCode}</p>
+            <p>{child.nftForDonation.description}</p>
+          </div>
+        </>))}
         <Home route="/" greeting="Welcome to Nullstack!" />
       </body>
     )
